c639c073bc91c0f4a7fd13ff9989863a1ec119b8
Dmitrii@Totskii <dmitry.totskiy@infovista.com>
2023-07-16 21:14:27 +0400

    Fix

--- server/src/domain/use-cases/render/getRenders/getRenders.ts
+++ server/src/domain/use-cases/render/getRenders/getRenders.ts
@@ -2,7 +2,6 @@ import { IServiceCradle, IUseCase } from "../../../../interface";
 import { IRenderDto } from "../../../entities/Render/IRender";
 import { IRenderMapper } from "../../../entities/Render/IRenderMapper";
 import { IRenderRepo } from "../../../entities/Render/IRenderRepo";
-import { Render } from "../../../entities/Render/Render";
 
 export class GetRendersUseCase implements IUseCase<void, IRenderDto[]> {
     private readonly repo: IRenderRepo;
@@ -14,7 +13,6 @@ export class GetRendersUseCase implements IUseCase<void, IRenderDto[]> {
     }
 
     public execute = async (): Promise<IRenderDto[]> => {
-        const renders = (await this.repo.find()).map(r => Render.Create(r.id, r.meta, r.name, r.src))
-        return Promise.resolve(renders.map(this.mapper.toDTO));
+        return (await Promise.resolve(await this.repo.find())).map(this.mapper.toDTO)
     }
 }
