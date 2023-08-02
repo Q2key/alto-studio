d78256a7fed2d4663bb17f5cbdaa93c74373c9b4
Dmitrii@Totskii <dmitry.totskiy@infovista.com>
2023-08-02 23:53:37 +0400

    Fix typo

--- server/src/domain/entities/Course/Course.ts
+++ server/src/domain/entities/Course/Course.ts
@@ -1,10 +1,9 @@
 import { ILesson } from "../Lesson/ILesson";
-import { ICourse } from "./ICource";
+import { ICourse } from "./ICourse";
 
 export class Course implements ICourse {
     id?: string;
     name: string;
     description: string;
     lessons: ILesson[];
-    
 }
