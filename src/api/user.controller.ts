8b6901f1bbe436d7b58ebad54ebcc564ba7c5915
dmtocque <dmitry.totskiy@fastdev.se>
2023-11-03 17:13:31 +0400

    minor change

--- src/api/user.controller.ts
+++ src/api/user.controller.ts
@@ -1,23 +1,22 @@
-import { Body, Controller, Get, Post } from "@nestjs/common";
-import { IUser } from "../domain/user/user.domain.interface";
-import { CreateUserDto } from "src/contracts/dto/create-user.dto";
-import { GetUsersUseCase } from "../use-cases/get-users/get-users.use-case";
-import { CreateUserUseCase } from "../use-cases/create-user/create-user.use-case";
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { IUser } from '../domain/user/user.domain.interface';
+import { CreateUserDto } from 'src/contracts/dto/create-user.dto';
+import { GetUsersUseCase } from '../use-cases/get-users/get-users.use-case';
+import { CreateUserUseCase } from '../use-cases/create-user/create-user.use-case';
 
 @Controller()
 export class UserController {
   constructor(
     private getUserUseCases: GetUsersUseCase,
-    private createUserUseCase: CreateUserUseCase
-  ) {
-  }
+    private createUserUseCase: CreateUserUseCase,
+  ) {}
 
-  @Get("users")
+  @Get('users')
   async findAll(): Promise<IUser[]> {
     return await this.getUserUseCases.execute();
   }
 
-  @Post("user")
+  @Post('user')
   async post(@Body() createCatDto: CreateUserDto): Promise<IUser> {
     return await this.createUserUseCase.execute(createCatDto);
   }
