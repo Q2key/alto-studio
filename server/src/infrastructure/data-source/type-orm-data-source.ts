61a81021d4fa6bf2d92eccd8c3893ddb14fe3a7a
dmtocque <dmitry.totskiy@fastdev.se>
2023-11-06 19:27:58 +0400

    Minor change for data-source

--- server/src/infrastructure/data-source/type-orm-data-source.ts
+++ server/src/infrastructure/data-source/type-orm-data-source.ts
@@ -23,12 +23,16 @@ export class TypeOrmDataSource implements GenericDbDataSource<DataSource> {
   }
 
   async initDataSource(): Promise<void> {
+    if (this.dataSource.isInitialized) {
+      throw new Error('DataSource is already initialized');
+    }
+
     await this.dataSource.initialize();
   }
 
   getDataSource(): DataSource {
     if (!this.dataSource.isInitialized) {
-      throw 'error!';
+      throw new Error('DataSource is not initialized');
     }
 
     return this.dataSource;
